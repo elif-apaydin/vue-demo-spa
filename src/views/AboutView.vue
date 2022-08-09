9d5f84327163d50ecfc068d11dd921324f979ce3
Elif Apaydın <79757022+elif-apaydin@users.noreply.github.com>
2022-08-09 12:12:19 +0300

    initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere tempora perspiciatis commodi quisquam sit eos maxime fuga at? Autem amet facilis laborum quod? Deleniti at consequuntur labore veritatis aliquam id.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint maxime hic, debitis facere consectetur corporis omnis incidunt ex. Cumque fugit architecto amet laborum aliquam sit earum ab eligendi, beatae commodi?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam eum aspernatur incidunt facere magni, sapiente, labore soluta perferendis enim consequatur excepturi ut voluptatem earum totam adipisci voluptatibus est nihil quos!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam officia animi eligendi qui quisquam nulla unde explicabo, fugit fuga deleniti, praesentium quasi pariatur dolore veritatis sed nisi distinctio adipisci non.</p>
+  </div>
+</template>
+
+<style scoped>
+.about{
+  max-width: 600px;
+  margin: 0px auto;
+}
+</style>
